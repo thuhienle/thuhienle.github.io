1ff03953eddba872c65b616777965ec10bca797a
Hien Le <hienthut@ualberta.ca>
2017-08-03 20:44:05 -0600

    try php

---
+++ learningphp.php
@@ -0,0 +1,22 @@
+/**
+ * Created by PhpStorm.
+ * User: hienle
+ * Date: 8/3/17
+ * Time: 8:29 PM
+ */
+
+<html>
+
+    <head>
+        <title>Information Gathered</title>
+    </head>
+
+    <body>
+
+    <?php
+
+    echo "<p>Data processed</p>";
+
+    ?>
+    </body>
+</html>
